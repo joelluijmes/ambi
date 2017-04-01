94b0694a096e52f3c5dee61c29e62add0b1ca850
Joël Luijmes <joelluijmes@gmail.com>
2017-04-01 13:58:03 +0200

    init commit (project creation)

---
+++ avr/ambi-avr/main.c
@@ -0,0 +1,18 @@
+/*
+ * ambi-avr.c
+ *
+ * Created: 4/1/2017 1:48:08 PM
+ * Author : joell
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
